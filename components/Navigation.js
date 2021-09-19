2dc8b00b1faa2415add680fe595e197a6252611f
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-19 12:34:55 +0200

    added navigation component and styles

---
+++ components/Navigation.js
@@ -0,0 +1,62 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { GrRun } from "react-icons/gr";
+import { GiRunningNinja } from "react-icons/gi";
+import { links, social } from "../data/nav_data.js";
+
+const Navigation = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <div className="nav-header-logo">
+            <h4>running-passion</h4>
+            <GiRunningNinja className="nav-header-icon" />
+          </div>
+
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
