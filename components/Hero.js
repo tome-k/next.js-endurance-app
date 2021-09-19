8385bfeb4b42a538b11aec7ec668dd9d502089de
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-19 13:11:13 +0200

    fixed bug with nav's properties

---
+++ components/Hero.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <div>
+      <Image src="/images/run_004.jpg" alt="landscape runner photo" />
+    </div>
+  );
+}
+
+export default Hero;
