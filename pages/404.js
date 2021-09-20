ce4259274dbd74ed6362b4a807e301826f64ebc2
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-20 18:18:07 +0200

    added new styles to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,27 @@
+import Link from "next/link";
+import styles from "../styles/404.module.css";
+import router from "next/router";
+import { useEffect } from "react";
+
 function NotFound404() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/");
+    }, 10000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
+    <div className={styles.container}>
       <h2>404</h2>
       <h4>page not found!</h4>
+      <small>
+        After 10 seconds you will be automatically redirected to the home page
+        or press it back{" "}
+      </small>
+      <Link href="/">
+        <a>go back</a>
+      </Link>
     </div>
   );
 }
