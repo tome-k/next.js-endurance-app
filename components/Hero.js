99452eba76b42b9eaf8bc6717f1f9c2355cdf033
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-23 16:48:07 +0200

    added left inner banner

--- components/Hero.js
+++ components/Hero.js
@@ -12,6 +12,11 @@ function Hero() {
         quality={100}
         className={styles.heroImage}
       />
+      <div className={styles.inner}>
+        <h4>Go For Running</h4>
+        <h2>Cracovia Marathon</h2>
+        <button>Run Now</button>
+      </div>
     </section>
   );
 }
