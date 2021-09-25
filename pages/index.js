494bc685e89ccff3349f1187548887cbdd94b076
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-25 14:08:04 +0200

    finished events component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Divider from "../components/Divider";
+import Events from "../components/Events";
 import Hero from "../components/Hero";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <Hero />
       <Divider />
+      <Events />
     </>
   );
 }
