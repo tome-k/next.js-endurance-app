a02b927f1f390fc9050823a853837e5feadd1cf9
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-29 19:29:58 +0200

    added bare bones contact page and styles

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,37 @@
+import styles from "../../styles/Contact.module.css";
+
 function ContactPage() {
   return (
-    <div>
-      <h3>Contact Page</h3>
-    </div>
+    <>
+      <section className={styles.contact}>
+        <div className={styles.container}>
+          <h3>Email Me</h3>
+          <form method="POST" action="send">
+            <div className={styles.control}>
+              <label>Full Name</label>
+              <input type="text" name="fullname" />
+            </div>
+            <div className={styles.control}>
+              <label>Email Address</label>
+              <input type="email" name="email" />
+            </div>
+            <div className={styles.control}>
+              <label>Phone Number</label>
+              <input type="text" name="phone" />
+            </div>
+            <div className={styles.control}>
+              <label>Message</label>
+              <textarea name="message" rows="5"></textarea>
+            </div>
+            <div className={styles.control}>
+              <button type="submit" className={styles.btn}>
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
 
