0110b97c4eaa8ea69d5390b19c9bd2499504333b
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-29 20:23:36 +0200

    added new image files and new styles

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,27 +1,25 @@
+import Divider from "../../components/Divider";
 import styles from "../../styles/Contact.module.css";
 
 function ContactPage() {
   return (
-    <>
+    <div className="">
+      <Divider />
       <section className={styles.contact}>
         <div className={styles.container}>
-          <h3>Email Me</h3>
+          <h3>Contact Us</h3>
           <form method="POST" action="send">
             <div className={styles.control}>
               <label>Full Name</label>
-              <input type="text" name="fullname" />
+              <input type="text" name="fullname" required />
             </div>
             <div className={styles.control}>
               <label>Email Address</label>
-              <input type="email" name="email" />
-            </div>
-            <div className={styles.control}>
-              <label>Phone Number</label>
-              <input type="text" name="phone" />
+              <input type="email" name="email" required />
             </div>
             <div className={styles.control}>
               <label>Message</label>
-              <textarea name="message" rows="5"></textarea>
+              <textarea name="message" rows="5" required></textarea>
             </div>
             <div className={styles.control}>
               <button type="submit" className={styles.btn}>
@@ -31,7 +29,8 @@ function ContactPage() {
           </form>
         </div>
       </section>
-    </>
+      <Divider />
+    </div>
   );
 }
 
