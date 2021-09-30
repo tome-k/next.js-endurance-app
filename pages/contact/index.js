4026c4640032792ec99ec24f67e9a92f7f3c1e9d
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-30 12:05:11 +0200

    refactored the code, added Form component and styles

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,34 +1,12 @@
 import Divider from "../../components/Divider";
+import Form from "../../components/Form";
 import styles from "../../styles/Contact.module.css";
 
 function ContactPage() {
   return (
     <div className="">
       <Divider />
-      <section className={styles.contact}>
-        <div className={styles.container}>
-          <h3>Contact Us</h3>
-          <form method="POST" action="send">
-            <div className={styles.control}>
-              <label>Full Name</label>
-              <input type="text" name="fullname" required />
-            </div>
-            <div className={styles.control}>
-              <label>Email Address</label>
-              <input type="email" name="email" required />
-            </div>
-            <div className={styles.control}>
-              <label>Message</label>
-              <textarea name="message" rows="5" required></textarea>
-            </div>
-            <div className={styles.control}>
-              <button type="submit" className={styles.btn}>
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </section>
+      <Form />
       <Divider />
     </div>
   );
