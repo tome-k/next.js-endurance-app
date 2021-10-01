48fc0303d2b7d6dc5af89f22337c8beda59e77c6
nicholas-bd <nicholas.bond.d@gmail.com>
2021-10-01 12:48:08 +0200

    added mapbox functionality

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,23 @@
 import styles from "../styles/Map.module.css";
+import ReactMapGL from "react-map-gl";
+import { useState } from "react";
 
 function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 50.06,
+    longitude: 19.911,
+    zoom: 14,
+    width: "100%",
+    height: "100%",
+  });
+
   return (
-    <div className={styles.map}>
-      <h3>this the map component</h3>
-    </div>
+    <ReactMapGL
+      mapStyle="mapbox://styles/mariog8/cku83ivq220v217o4zvc5frcr"
+      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+      className={styles.map}
+      {...viewport}
+    ></ReactMapGL>
   );
 }
 
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,19 +1,19 @@
 import Divider from "../../components/Divider";
 import Form from "../../components/Form";
 import Map from "../../components/Map";
+import styles from "../../styles/Contact.module.css";
 
 function ContactPage() {
   return (
-    <>
-      <Map />
-      <Divider img="/images/run_02.jpg" title="Let's run!" position="bottom" />
-      <Form />
-      <Divider
+    <section className={styles.contact}>
+      {/* <Divider
         img="/images/run_24.jpg"
         title="Get some endorphin!"
         position="center"
-      />
-    </>
+      /> */}
+      <Map />
+      {/* <Form /> */}
+    </section>
   );
 }
 
