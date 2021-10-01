155fa55491d311445e63e52af41bbc2f44707c2f
nicholas-bd <nicholas.bond.d@gmail.com>
2021-10-01 14:46:40 +0200

    added marker and fixed bugs with styles

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -6,12 +6,15 @@ import styles from "../../styles/Contact.module.css";
 function ContactPage() {
   return (
     <section className={styles.contact}>
-      {/* <Divider
+      <h4>Here You can reach Us</h4>
+      <h5>every wednesday and sunday</h5>
+      <h6>at 6 PM</h6>
+      <Map />
+      <Divider
         img="/images/run_24.jpg"
         title="Get some endorphin!"
         position="center"
-      /> */}
-      <Map />
+      />
       {/* <Form /> */}
     </section>
   );
