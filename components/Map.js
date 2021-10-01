005c4c8f3176696db9da9c4637b579e23a17b4cf
nicholas-bd <nicholas.bond.d@gmail.com>
2021-10-01 10:40:16 +0200

    added map component barebones

---
+++ components/Map.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Map.module.css";
+
+function Map() {
+  return (
+    <div className={styles.map}>
+      <h3>this the map component</h3>
+    </div>
+  );
+}
+
+export default Map;
