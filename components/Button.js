732d6061c32f9f957cfcd4ec493e0c6f6e38b58d
nicholas-bd <nicholas.bond.d@gmail.com>
2021-10-03 16:12:29 +0200

    added new Button component and styles

---
+++ components/Button.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "../styles/Button.module.css";
+
+function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={styles.btn} target="_blank" rel="noreferrer">
+          {props.children}
+        </a>
+      </Link>
+    );
+  }
+
+  return (
+    <button className={styles.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
