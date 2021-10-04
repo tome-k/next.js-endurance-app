e326e6f2acab724b72a0d6532b9714f3e304b695
nicholas-bd <nicholas.bond.d@gmail.com>
2021-10-04 10:51:15 +0200

    added new images

--- pages/index.js
+++ pages/index.js
@@ -25,25 +25,25 @@ export default function Home({ data }) {
       />
       <TestimonialsWrapper>
         <Testimonial
-          img="/images/shoe_005.jpg"
+          img="/images/runner_000.jpg"
           name="Mario Golik"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
           desc="marathoner"
         />
         <Testimonial
-          img="/images/shoe_001.jpg"
+          img="/images/runner_001.jpg"
           name="Mario Golik"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
           desc="marathoner"
         />
         <Testimonial
-          img="/images/shoe_004.jpg"
+          img="/images/runner_002.jpg"
           name="Mario Golik"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
           desc="marathoner"
         />
         <Testimonial
-          img="/images/shoe_003.jpg"
+          img="/images/runner_003.jpg"
           name="Mario Golik"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas amet deleniti velit omnis doloremque aliquid, ipsum temporibus ullam quia repellendus."
           desc="marathoner"
