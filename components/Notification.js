d969b19bbda284374b180108a0ad4b49aeb32df6
nicholas-bd <nicholas.bond.d@gmail.com>
2021-10-17 19:11:32 +0200

    added Notification component and styles, prepare context api

---
+++ components/Notification.js
@@ -0,0 +1,33 @@
+import styles from "../styles/Notification.module.css";
+import { useContext } from "react";
+
+function Notification(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const { title, message, status } = props;
+
+  let statusClasses = "";
+
+  if (status === "success") {
+    statusClasses = classes.success;
+  }
+
+  if (status === "error") {
+    statusClasses = classes.error;
+  }
+
+  if (status === "pending") {
+    statusClasses = classes.pending;
+  }
+
+  const activeClasses = `${classes.notification} ${statusClasses}`;
+
+  return (
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default Notification;
