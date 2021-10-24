c7862848d221c7e2ea5839ca6024773dc16891d9
nicholas-bd <nicholas.bond.d@gmail.com>
2021-10-24 19:51:21 +0200

    added single post blog bare bones

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,50 @@
-function SingleBlogPostPage() {
-  return <div>Enter</div>;
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+function SingleBlogPostPage({
+  frontmatter: { title, category, date, cover_image, author, author_image },
+  content,
+  slug,
+}) {
+  return (
+    <div>
+      <h3>{date}</h3>
+      <p>{author}</p>
+    </div>
+  );
 }
+
+export const getStaticPaths = async () => {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const markdownWithMeta = fs.readFileSync(
+    path.join("posts", slug + ".md"),
+    "utf-8"
+  );
+
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      frontmatter,
+      content,
+      slug,
+    },
+  };
+};
 
 export default SingleBlogPostPage;
