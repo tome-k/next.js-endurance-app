b784e302381a097cf14ed745cce095acf22e434d
nicholas-bd <nicholas.bond.d@gmail.com>
2021-11-07 12:05:08 +0100

    added new components and styles backdrop & modal

---
+++ components/Modal.js
@@ -0,0 +1,25 @@
+import styles from "../styles/Modal.module.css";
+
+function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+
+  function confirmHandler() {
+    props.onConfirm();
+  }
+
+  return (
+    <div className={styles.modal}>
+      <p>Are you sure?</p>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        Cancel
+      </button>
+      <button className="btn" onClick={confirmHandler}>
+        Confirm
+      </button>
+    </div>
+  );
+}
+
+export default Modal;
--- pages/plans/index.js
+++ pages/plans/index.js
@@ -1,6 +1,19 @@
 import Head from "next/head";
+import { useState } from "react";
+import Backdrop from "../../components/Backdrop";
+import Modal from "../../components/Modal";
 
-function PlansPage() {
+function PlansPage(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <>
       <Head>
@@ -12,6 +25,18 @@ function PlansPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h3>plans page</h3>
+      <div className="card">
+        <h2>{props.text}</h2>
+        <div className="actions">
+          <button className="btn" onClick={deleteHandler}>
+            Delete
+          </button>
+        </div>
+        {modalIsOpen && (
+          <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+        )}
+        {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+      </div>
     </>
   );
 }
