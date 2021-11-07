2da6072b9e24425c094d0e8887cda868859ca01d
nicholas-bd <nicholas.bond.d@gmail.com>
2021-11-07 17:28:04 +0100

    added new image files and fixed styles bugs

--- pages/plans/index.js
+++ pages/plans/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import PlansCard from "../../components/PlansCard";
+import styles from "../../styles/PlansPage.module.css";
 
 function PlansPage() {
   return (
@@ -12,10 +13,10 @@ function PlansPage() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h3>plans page</h3>
-      <PlansCard text="42,195m" img="/images/run_13.jpg" />
-      <PlansCard text="21,0975m" img="/images/run_14.jpg" />
-      <PlansCard text="10,000m" img="/images/run_15.jpg" />
+      <h3 className={styles.heading}>Chose your plan</h3>
+      <PlansCard text="42,195m" img="/images/marathon_plan.jpg" />
+      <PlansCard text="21,0975m" img="/images/half_marathon_plan.jpg" />
+      <PlansCard text="10,000m" img="/images/10k-plan.jpg" />
     </>
   );
 }
